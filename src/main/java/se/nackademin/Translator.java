9611a700eb12eacf7346f1fbe0a0880f80d12ef6
Christoffer ~Fynrir~ Johansson <christoffer.johansson@yh.nackademin.se>
2016-04-04 18:51:36 +0200

    Changelog:
    Work (wörk) has continued on the translator class.
    It now contains 100% more file reading (shredding :O) capabilities. 
    It's just a shame I got no good way to print the entire array that results
    from the setOrdlista() method in a human readable format without having to code like fuck.
    
    Last thing to do here is making a get method that retrivies the words we want depending on the numbers that was calculated
    from that MagicNumbers.java class. :D

--- src/main/java/se/nackademin/Translator.java
+++ src/main/java/se/nackademin/Translator.java
@@ -5,6 +5,10 @@
  */
 package se.nackademin;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author cbg-fynrir
@@ -27,11 +31,26 @@ public class Translator {
      * 2
      * ..
      * 9
+     * 
      */
     
     // On the top row, 0 is Adjektiv, 1 is Substantiv, and 2 is a Verb.
     //Filling the Ordlista now.
     
+    //Getters for this class:
+    
+    /**
+     * 
+     * @return String
+     */
+    
+//    public String[] getWords() {
+//        String[] word;
+//        
+//        
+//        
+//        return "Nothing";
+//    }
     
     //Setters for this class:
 
@@ -61,8 +80,50 @@ public class Translator {
     
     //Fill the Ordlista:
     
-    public void setOrdlista() {
-        //This method sets ordlista, which is hardcoded. It will not have any parameters.
+    public void setOrdlista() throws IOException {
+        
+        FileReader fileReader = new FileReader("data/adjektiv");  
+        int i = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line = null;
+            
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                this.Ordlista[i][0] = line;
+                i++;
+            }
+            
+            bufferedReader.close();
+        }
+        
+        
+        
+        FileReader fileReader2 = new FileReader("data/substantiv");  
+        i = 0;
+        try (BufferedReader bufferedReader2 = new BufferedReader(fileReader2)) {
+            String line = null;
+            
+            while ((line = bufferedReader2.readLine()) != null)
+            {
+                this.Ordlista[i][1] = line;
+                i++;
+            }
+            bufferedReader2.close();
+        }
+        
+         FileReader fileReader3 = new FileReader("data/verb");  
+        i = 0;
+        try (BufferedReader bufferedReader3 = new BufferedReader(fileReader3)) {
+            String line = null;
+            
+            while ((line = bufferedReader3.readLine()) != null)
+            {
+                this.Ordlista[i][2] = line;
+                i++;
+            }
+            bufferedReader3.close();
+        }
+        
     }
     
     
